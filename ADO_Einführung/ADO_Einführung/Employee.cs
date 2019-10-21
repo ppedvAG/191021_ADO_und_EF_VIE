4bdcdad59b407a1048df69497d82bf9fdcc15e06
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-21 12:23:23 +0200

    Daten aus der DB lesen

---
+++ ADO_Einführung/ADO_Einführung/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Einführung
+{
+    class Employee
+    {
+        public int? ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
--- ADO_Einführung/ADO_Einführung/Program.cs
+++ ADO_Einführung/ADO_Einführung/Program.cs
@@ -31,20 +31,53 @@ namespace ADO_Einführung
             // ConnectionString für Trainer-PC:
             string connectionString = @"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=True;";
 
-            var con = new SqlConnection(connectionString);
+            // Variante 1)
+            #region Variante 1 ohne using
+            //var con = new SqlConnection(connectionString);
 
-            con.Open();
-            Console.WriteLine("Yeah !");
+            //con.Open();
+            //Console.WriteLine("Yeah !");
 
-            var command = new SqlCommand();
-            command.Connection = con;
-            command.CommandText = "SELECT count(*) FROM Employees";
+            //var command = new SqlCommand();
+            //command.Connection = con;
+            //command.CommandText = "SELECT count(*) FROM Employees";
 
-            // command.ExecuteNonQuery // <--- Befehl absetzen, ohne Ergebnis (z.B. Stored Proc, INSERT usw...)
-            // command.ExecuteReader // <--- Mehrere Ergebnisse
-            int result = (int)command.ExecuteScalar(); // <--- Ein einzelnes Feld/Ergebnis
+            //// command.ExecuteNonQuery // <--- Befehl absetzen, ohne Ergebnis (z.B. Stored Proc, INSERT usw...)
+            //// command.ExecuteReader // <--- Mehrere Ergebnisse
+            //int result = (int)command.ExecuteScalar(); // <--- Ein einzelnes Feld/Ergebnis
 
-            Console.WriteLine($"Es gibt {result} Employees in der DB");
+            //Console.WriteLine($"Es gibt {result} Employees in der DB");
+            //con.Close(); // Verbindung wieder schließen
+            #endregion
+            // Variante 2)
+
+            using(var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                // Alternativer Aufbau eines Commands:
+                using(var command = con.CreateCommand()) // Setzt das Connection-Property automatisch
+                {
+                    command.CommandText = "SELECT * FROM Employees";
+                    var reader = command.ExecuteReader();
+                    List<Employee> employees = new List<Employee>();
+                    while(reader.Read())
+                    {
+                        // DTO -> Data Transfer Object
+                        Employee emp = new Employee();
+                        emp.ID = reader.GetInt32(reader.GetOrdinal("EmployeeID")); // GetOrdinal ermittelt die Spaltennummer aus dem Namen
+                        emp.FirstName = (string)reader["FirstName"]; // Mit Indexer auf die Spalte direkt zugreifen
+                        emp.LastName = reader.GetString(1);
+                        emp.BirthDate = reader.GetDateTime(5);
+                        emp.HireDate = reader.GetDateTime(reader.GetOrdinal("HireDate"));
+
+                        employees.Add(emp);
+                        Console.WriteLine($"Name: {emp.LastName}\tBirth:{emp.BirthDate:d}\tHire:{emp.HireDate:d}");
+                    }
+                    // Man könnte hier zb mit der Liste weiterarbeiten
+                }
+
+            } // con.Close();
+
 
 
             Console.WriteLine("---ENDE---");
