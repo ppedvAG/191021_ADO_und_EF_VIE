b1dd8b4ed8c57d6d5b31e5dba32ee2230532be88
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-23 15:24:13 +0200

    Daten laden mit CodeFirst

--- Hallo_CodeFirst/Hallo_CodeFirst/MainWindow.xaml.cs
+++ Hallo_CodeFirst/Hallo_CodeFirst/MainWindow.xaml.cs
@@ -37,7 +37,17 @@ namespace Hallo_CodeFirst
 
         private void DemoDaten_Click(object sender, RoutedEventArgs e)
         {
+            context.Buch.Add(new Buch { Titel = "Mein kleiner Garten", Autor = "Peter Silie", Seiten = 33, Preis = 35m });
+            context.Buch.Add(new Buch { Titel = "Reiseführer Paris", Autor = "Franz Ose", Seiten = 120, Preis = 15.90m });
+            context.Buch.Add(new Buch { Titel = "Kindererziehung leicht gemacht", Autor = "Bill Dung", Seiten = 2800, Preis = 248.40m });
+            context.Buch.Add(new Buch { Titel = "Frauen verstehen Band 3 von 293", Autor = "Anna Bolika", Seiten = 800, Preis = 9.99m });
+            context.Buch.Add(new Buch { Titel = "Vogelkunde", Autor = "Albert Tross", Seiten = 50, Preis = 19m });
 
+            context.Buchgeschäft.Add(new Buchgeschäft { Name = "Thalia", Adresse = "Thaliastraße 100" });
+            context.Buchgeschäft.Add(new Buchgeschäft { Name = "Morawa", Adresse = "SCS 123456" });
+            context.Buchgeschäft.Add(new Buchgeschäft { Name = "Tante Emmas Bücherkiste", Adresse = "Daham 12" });
+
+            context.SaveChanges();
         }
 
 
