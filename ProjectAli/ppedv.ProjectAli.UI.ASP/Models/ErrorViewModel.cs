b2f7a00b9fdf1a3a315583132049b37ba225d4c0
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-25 13:50:42 +0200

    Init ASP

---
+++ ProjectAli/ppedv.ProjectAli.UI.ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ppedv.ProjectAli.UI.ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
